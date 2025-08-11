073cb0675338914105c11136bc698c941439f946
svkhrobbeck <svkhrobbeck@gmail.com>
2025-08-11 11:33:45 +0500

    feat: :sparkles: add refresh_token feature

--- src/modules/uzum-market-api/core/auth/params.ts
+++ src/modules/uzum-market-api/core/auth/params.ts
@@ -1,1 +1,6 @@
+import { UsernamePasswordParams } from "@/common";
 
+export interface TokenParams extends UsernamePasswordParams {
+  grant_type?: "password" | "refresh_token";
+  refresh_token?: string;
+}
