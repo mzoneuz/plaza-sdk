d01b5aa69bbb11746f5c7a6b978df700f4fedbad
svkhrobbeck <svkhrobbeck@gmail.com>
2025-08-19 16:15:42 +0500

    feat: :sparkles: add changes

--- src/modules/alif-shop-client-api/core/auth/api.ts
+++ src/modules/alif-shop-client-api/core/auth/api.ts
@@ -1,10 +1,10 @@
 import { CallerPayload } from "@/common";
 
 export const RequestLogin: CallerPayload<{ phone: string }> = (http, payload) => {
-  return http.post("/auth/request-login", payload, { headers: { "accept-language": "uz", "service-token": "service-token-alifshop" } });
+  return http.post("/auth/request-login", payload);
 };
 
 export const Login: CallerPayload<{ name?: string; otp: string; phone: string; remember_me: boolean }> = (http, payload) => {
   const { name = "", ...restPayload } = payload;
-  return http.post("/auth/login", { name, ...restPayload }, { headers: { "accept-language": "uz", "service-token": "service-token-alifshop" } });
+  return http.post("/auth/login", { name, ...restPayload });
 };
