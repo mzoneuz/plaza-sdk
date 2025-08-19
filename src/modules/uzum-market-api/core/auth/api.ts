4537563e256375eae05ebb01b346b1c324a5d8d7
svkhrobbeck <svkhrobbeck@gmail.com>
2025-08-19 16:11:01 +0500

    feat: :sparkles: add alif-shop-client-api

--- src/modules/uzum-market-api/core/auth/api.ts
+++ src/modules/uzum-market-api/core/auth/api.ts
@@ -5,14 +5,14 @@ import { GetTokenParams, RefreshTokenParams } from "./params";
 
 const headers = { "content-type": "application/x-www-form-urlencoded", authorization: `Basic YjJiLWZyb250OmNsaWVudFNlY3JldA==` };
 
-export const GetToken: CallerParam<GetTokenParams, Types.AuthApi.GetToken.Response> = async (uzumHttp, params) => {
-  return uzumHttp.post("/oauth/token", { grant_type: "password", username: params.username, password: params.password, referer: "" }, { headers });
+export const GetToken: CallerParam<GetTokenParams, Types.AuthApi.GetToken.Response> = async (http, params) => {
+  return http.post("/oauth/token", { grant_type: "password", username: params.username, password: params.password, referer: "" }, { headers });
 };
 
-export const RefreshToken: CallerParam<RefreshTokenParams, Types.AuthApi.RefreshToken.Response> = async (uzumHttp, params) => {
-  return uzumHttp.post("/oauth/token", { grant_type: "refresh_token", refresh_token: params.refresh_token, referer: "" }, { headers });
+export const RefreshToken: CallerParam<RefreshTokenParams, Types.AuthApi.RefreshToken.Response> = async (http, params) => {
+  return http.post("/oauth/token", { grant_type: "refresh_token", refresh_token: params.refresh_token, referer: "" }, { headers });
 };
 
-export const CheckToken: CallerParam<string, Types.AuthApi.CheckToken.Response> = async (uzumHttp, token: string) => {
-  return uzumHttp.post("/auth/seller/check_token", { token }, { headers });
+export const CheckToken: CallerParam<string, Types.AuthApi.CheckToken.Response> = async (http, token: string) => {
+  return http.post("/auth/seller/check_token", { token }, { headers });
 };
