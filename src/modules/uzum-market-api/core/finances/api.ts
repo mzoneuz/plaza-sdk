41fff6393fde0bf49a5cb6004bd7318ffcfcfb00
svkhrobbeck <svkhrobbeck@gmail.com>
2025-09-25 12:44:53 +0500

    feat: :sparkles: add changes

--- src/modules/uzum-market-api/core/finances/api.ts
+++ src/modules/uzum-market-api/core/finances/api.ts
@@ -1,6 +1,8 @@
 import { CallerParam, pasteAuthHeader } from "@/common";
-import * as Params from "./params";
 
-export const ExpensesList: CallerParam<Params.ExpensesListParams> = async (http, params) => {
+import * as Params from "./params";
+import * as Types from "./types";
+
+export const ExpensesList: CallerParam<Params.ExpensesListParams, Types.FinancesApi.ExpensesList.Response> = async (http, params) => {
   return http.get("/seller/finance/expenses", { params, headers: pasteAuthHeader(params?.token) });
 };
--- src/modules/uzum-market-api/core/finances/types.ts
+++ src/modules/uzum-market-api/core/finances/types.ts
@@ -1,7 +1,29 @@
+export declare namespace FinancesTypes {
+  export interface Expense {
+    id: number;
+    dateCreated: number;
+    dateUpdated: unknown | null;
+    name: string;
+    source: string;
+    shopId: number;
+    sellerId: number;
+    paymentPrice: number;
+    amount: number;
+    status: string;
+    externalId: string;
+    code: string;
+    dateService: number;
+    type: string;
+  }
+}
+
 export declare namespace FinancesApi {
   export namespace ExpensesList {
     export interface Response {
-      data: unknown[];
+      payload: {
+        payments: FinancesTypes.Expense[];
+      };
+      timestamp: string;
     }
   }
 }
