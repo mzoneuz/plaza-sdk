0a6d32b05a2868e6a037876f01e2cbc3dfd5f0d7
tofim <tofim@gmail.com>
2025-11-22 17:16:19 +0500

    fix: :ambulance: make optional generate report lang param

--- src/modules/yandex-market-api/core/reports/params.ts
+++ src/modules/yandex-market-api/core/reports/params.ts
@@ -2,5 +2,5 @@ import { ApiKeyParams } from "@/common";
 
 export interface GenerateServiceReportParams extends ApiKeyParams {
   format?: "FILE" | "CSV" | "JSON";
-  language: "EN" | "RU";
+  language?: "EN" | "RU";
 }
